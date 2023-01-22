f6859cf3dd0f3327f49f6b23d23d80a18c26b9ee
ilkochw <ilkochw@gmail.com>
2023-01-22 13:06:16 +0300

    added simple logging

--- app/food_api/api.py
+++ app/food_api/api.py
@@ -1,7 +1,7 @@
 from ninja import Router, Query
 from food_api.models import Topping, Food, FoodCategory
 from food_api.schemas import FoodCategorySchema, FoodFilterSchema, ToppingFilterSchema
-
+from loguru import logger
 
 router = Router()
 
@@ -10,30 +10,32 @@ router = Router()
 def list_foods(request, food_filters: FoodFilterSchema = Query(...), topping_filters: ToppingFilterSchema = Query(...)):
 
     topping_filters_dict = topping_filters.dict(exclude_none=True)
-
+    logger.debug(f'Topping filters: {topping_filters_dict}')
     food_filters_dict = food_filters.dict(exclude_none=True)
     food_filters_dict['is_publish'] = True
     if topping_filters_dict:
         toppings = Topping.objects.filter(**topping_filters_dict)
-
         food_filters_dict['toppings__in'] = toppings
 
-    foods = Food.objects.filter(**food_filters_dict)
+    logger.debug(f'Food filters: {food_filters_dict}')
 
+    foods = Food.objects.filter(**food_filters_dict)
     food_ids = list(foods.values_list('pk', flat=True))
 
     categories = FoodCategory.objects.filter(is_publish=True)
+
     fields = ['id', 'name']
-
-    result = []
-    for cat in categories:
-        cat_json = {}
+    response = []
+    for category in categories:
+        category_dict = {}
         for attr in fields:
-            cat_json[attr] = cat.__dict__[attr]
+            category_dict[attr] = category.__dict__[attr]
 
-        foods_obj = list(cat.get_filtered_foods(food_ids))
+        foods_obj = list(category.get_filtered_foods(food_ids))
         if foods_obj:
-            cat_json['foods'] = foods_obj
-            result.append(cat_json)
+            category_dict['foods'] = foods_obj
+            response.append(category_dict)
 
-    return result
+    logger.debug(f'response GET api/foods: {response}')
+
+    return response
